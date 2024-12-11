76c30b58830bcdf8cf6459d68f48bfee283f4584
FIK3021 <157036535+FIK3021@users.noreply.github.com>
2024-12-12 00:21:47 +0700

    Update userRoutes.js

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const multer = require('multer');
-const { step1, step2, step3, step4 } = require('/Login Register/src/controllers/userController'); // Ganti dengan path ke file backend yang sudah dibuat
+const { register } = require('/Login Register/src/controllers/userController'); 
 
 const router = express.Router();
 const upload = multer();
 
-router.post('/register/step1', express.json(), step1);
-router.post('/register/step2', express.json(), step2);
-router.post('/register/step3/:uid', upload.single('profilePicture'), step3);
-router.post('/register/step4/:uid', upload.single('idCard'), step4);
+router.post('/register', 
+    upload.fields([
+        { name: 'profilePicture', maxCount: 1 },
+        { name: 'idCard', maxCount: 1 }
+    ]), 
+    register 
+);
 
 module.exports = router;
